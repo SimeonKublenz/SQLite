84b5a8c9d5a1a76a6b1eda44b66c2e580a6c75cb
SimeonKublenz <simeon2@schule.bremen.de>
2015-03-18 15:05:21 +0100

    Erste Vorversion der GUI 	- Layouts 	- JMenu

--- SQLite/src/de/szut/dqi12/sqlitebrowser/view/View.java
+++ SQLite/src/de/szut/dqi12/sqlitebrowser/view/View.java
@@ -1,23 +1,73 @@
 package de.szut.dqi12.sqlitebrowser.view;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.SwingConstants;
 
 import settings.*;
 
 public class View extends JFrame {
+	
+	private static final long serialVersionUID = -1206806827748738729L;
+	private JPanel mainPanel;
+	
 	
 	public static void main(String[] args) {
 		new View();
 	}
 	
 	public View() {
+		setTitle(Settings.NAME);
+		setBounds((Settings.SCREENWIDTH - Settings.WINDOWWIDTH)/2,
+				(Settings.SCREENHEIGHT - Settings.WINDOWHEIGHT)/2,
+				Settings.WINDOWWIDTH, Settings.WINDOWHEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(Settings.SCREENWIDTH, Settings.SCREENHEIGHT, 800, 620);
-		setTitle("SQLite");
-		setLayout(new BorderLayout());
+		mainPanel = createMainPanel();
+		setContentPane(mainPanel);
 		setVisible(true);
+	}
+	
+	public JPanel createMainPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout(0, 0));
+		JPanel menuPanel = new JPanel();
+		menuPanel.setLayout(new GridLayout(2, 1));
+		JPanel dataPanel = new JPanel();
+		dataPanel.setLayout(new BorderLayout(0, 0));
+		JPanel limitPanel = new JPanel();
+		limitPanel.setLayout(new GridLayout(1, 3, 0, 10));
+		panel.add(menuPanel, SwingConstants.NORTH);
+		panel.add(dataPanel, SwingConstants.CENTER);
+		dataPanel.add(limitPanel, SwingConstants.SOUTH);
 		
+		
+		JLabel connection = new JLabel("not connected");
+		connection.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JTree dataBaseTree = new JTree();
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu file = new JMenu();
+		JMenu edit = new JMenu();
+		JMenu help = new JMenu();
+		JMenuItem openDataBase = new JMenuItem();
+		menuBar.add(file);
+		menuBar.add(edit);
+		menuBar.add(help);
+		file.add(openDataBase);
+		
+		panel.add(connection, BorderLayout.SOUTH);
+		panel.add(dataBaseTree, BorderLayout.WEST);
+		dataPanel.add()
+		
+		return panel;
 	}
 }
--- SQLite/src/settings/Settings.java
+++ SQLite/src/settings/Settings.java
@@ -5,4 +5,7 @@ import java.awt.Toolkit;
 public class Settings {
 	public static int SCREENWIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
 	public static int SCREENHEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+	public static String NAME = "SQLite";
+	public static int WINDOWWIDTH = 800;
+	public static int WINDOWHEIGHT = 620;
 }
